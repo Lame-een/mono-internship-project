375eeb143cae5182a40b2e733cbbafce8078e6be
Lame-een <43285057+Lame-een@users.noreply.github.com>
2021-08-30 08:58:29 +0200

    feat: add method for sql null parameters

---
+++ src/Lyre.Repository.Common/SqlUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Lyre.Repository
+{
+    public static class SqlUtilities
+    {
+        public static void AddParameterWithNullableValue(SqlCommand command, string parameterName, object value)
+        {
+            if (value == null)
+            {
+                command.Parameters.AddWithValue(parameterName, DBNull.Value);
+            }
+            else
+            {
+                command.Parameters.AddWithValue(parameterName, value);
+            }
+        }
+    }
+}
