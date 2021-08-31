83d0da81ed01c436daa3af0d755f83992aa062c3
Lame-een <43285057+Lame-een@users.noreply.github.com>
2021-08-31 10:10:22 +0200

    feat: add Pager, Sorter classes & Filter interface
    Added pager and sorter classes and a filter interface which are used in
    SQL commands.

---
+++ src/Lyre.Common/IFilter.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+
+namespace Lyre.Common
+{
+    public interface IFilter
+    {
+        string FilterString { get; set; }
+        string GetSql();
+        void AddParameters(SqlCommand command);
+    }
+}
---
+++ src/Lyre.Common/Pager.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+
+namespace Lyre.Common
+{
+    public class Pager
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+
+        public Pager(int pageNumber = 1, int pageSize = 10)
+        {
+            if (pageSize < 5)
+            {
+                pageSize = 10;
+            }
+
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+        }
+
+        public int Offset { get => (PageNumber - 1) * PageSize; }
+
+        public string GetSql()
+        {
+            return " OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY ";
+        }
+
+        public void AddParameters(SqlCommand command)
+        {
+            command.Parameters.AddWithValue("@Offset", Offset);
+            command.Parameters.AddWithValue("@PageSize", PageSize);
+        }
+    }
+}
