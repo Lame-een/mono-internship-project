dc213c1bc56b5b518d75de192110db8a9595d594
Anita Hancic <hancic.anita@gmail.com>
2021-09-02 11:43:07 +0200

    feat: add Lyrics
    Added model, repository, service and controller for Lyrics.
    Needs testing once joins are figured out.
    Modified controllers for Genre and Artist.

---
+++ src/Lyre.Service/LyricsService.cs
@@ -0,0 +1,40 @@
+﻿using Lyre.Model.Common;
+using Lyre.Repository.Common;
+using Lyre.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyre.Service
+{
+    class LyricsService: ILyricsService
+    {
+        public ILyricsRepository LyricsRepo { get; set; }
+
+        public LyricsService(ILyricsRepository lyricsRepo)
+        {
+            LyricsRepo = lyricsRepo;
+        }
+
+        public async Task<int> PostLyricsAsync(ILyrics newLyrics)
+        {
+            newLyrics.ID = Guid.NewGuid();
+            newLyrics.CreationTime = DateTime.Now;
+            return await LyricsRepo.PostLyricsAsync(newLyrics);
+        }
+        public async Task<ILyrics> GetLyricsByIDAsync(Guid id)
+        {
+            return await LyricsRepo.GetLyricsByIDAsync(id);
+        }
+        public async Task<int> PutLyricsAsync(ILyrics lyrics)
+        {
+            return await LyricsRepo.PutLyricsAsync(lyrics);
+        }
+        public async Task<int> DeleteLyricsByIDAsync(Guid id)
+        {
+            return await LyricsRepo.DeleteLyricsByIDAsync(id);
+        }
+    }
+}
---
+++ src/Lyre.WebApi/Controllers/LyricsController.cs
@@ -0,0 +1,124 @@
+﻿using AutoMapper;
+using Lyre.Model;
+using Lyre.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Threading.Tasks;
+
+namespace Lyre.WebApi.Controllers
+{
+    public class LyricsController : ApiController
+    {
+        private ILyricsService Service { get; set; }
+        private IMapper Mapper { get; set; }
+
+        public LyricsController() { }
+        public LyricsController(ILyricsService service, IMapper mapper) 
+        {
+            Service = service;
+            Mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("api/Lyrics/{id}")]
+        public async Task<HttpResponseMessage> GetLyricsByIDAsync(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "No entries found.");
+            }
+
+            LyricsREST lyrics = Mapper.Map<LyricsREST>(await Service.GetLyricsByIDAsync(id));
+            if(lyrics == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "No rows found.");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, lyrics);
+        }
+
+        [HttpPost]
+        [Route("api/Lyrics")]
+        public async Task<HttpResponseMessage> PostLyricsAsync([FromBody] LyricsREST newLyrics)
+        {
+            if(newLyrics == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Body has invalid data.");
+            }
+
+            int status = await Service.PostLyricsAsync(new Lyrics(newLyrics.Text, newLyrics.UserID, newLyrics.SongID));
+            if(status == -1)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Body has invalid data.");
+            }
+            return Request.CreateResponse(HttpStatusCode.Created, "Inserted " + status.ToString() + " row(s).");
+        }
+
+        [HttpPut]
+        [Route("api/Lyrics")]
+        public async Task<HttpResponseMessage> PutLyricsAsync([FromBody] LyricsREST lyrics)
+        {
+            if (lyrics == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Body has invalid data.");
+            }
+
+            int status = await Service.PutLyricsAsync(new Lyrics(lyrics.ID, lyrics.Text, lyrics.UserID,
+                                                        lyrics.SongID, lyrics.CreationTime, lyrics.Verified));
+            if (status == -1)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Body has invalid data.");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, "Updated " + status.ToString() + " row(s).");
+        }
+
+        [HttpDelete]
+        [Route("api/Lyrics/{id}")]
+        public async Task<HttpResponseMessage> DeleteLyricsByIDAsync(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "No entries found.");
+            }
+
+            int status = await Service.DeleteLyricsByIDAsync(id);
+            if (status == -1)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Body has invalid data.");
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, "Deleted " + status.ToString() + " row(s).");
+        }
+
+        public class LyricsREST
+        {
+            public Guid ID { get; set; }
+            public string Text { get; set; }
+            public Guid UserID { get; set; }
+            public Guid SongID { get; set; }
+            public DateTime CreationTime { get; set; }
+            public char Verified { get; set; }
+
+            public LyricsREST() { }
+            public LyricsREST(string text, Guid userID, Guid songID, char verified = 'n')
+            {
+                Text = text;
+                UserID = userID;
+                SongID = songID;
+                Verified = verified;
+            }
+            public LyricsREST(Guid id, string text, Guid userID, Guid songID, DateTime creationTime, char verified = 'n')
+            {
+                ID = id;
+                Text = text;
+                UserID = userID;
+                SongID = songID;
+                CreationTime = creationTime;
+                Verified = verified;
+            }
+        }
+    }
+}
