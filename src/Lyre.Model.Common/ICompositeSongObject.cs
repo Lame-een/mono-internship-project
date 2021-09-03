25d525daef9d4107111e122123622fad77ea5376
Karlo Matijević <karlo.mat19@gmail.com>
2021-09-03 08:50:54 +0200

    feat: start of Join Table Issue
    CompositeSongObject is a new class in Model and Model.Common.

---
+++ src/Lyre.Model.Common/ICompositeSongObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyre.Model.Common
+{
+    public interface ICompositeSongObject
+    {
+        string song_name { get; set; }
+
+        string album_name { get; set; }
+
+        string artist_name { get; set; }
+
+        string genre_name { get; set; }
+    }
+}
