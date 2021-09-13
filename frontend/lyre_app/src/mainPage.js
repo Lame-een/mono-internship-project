9ec813011d8358acacda68459973ffb185ca4df5
Karlo Matijević <karlo.mat19@gmail.com>
2021-09-13 11:25:33 +0200

    Init frontend commit

---
+++ frontend/lyre_app/src/mainPage.js
@@ -0,0 +1,14 @@
+function mainPage() {
+    return(
+        
+        <div class="input-group" className = "mainPage">
+            <h1>Texst</h1>
+          <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+            aria-describedby="search-addon" />
+          <button type="button" class="btn btn-outline-primary">search</button>
+        </div>
+    )
+}
+
+
+export default mainPage;
